31ec603612e395d7b11ef51b95927d2b00bde7ac
Darek Ładno <darek0511@gmail.com>
2025-06-29 11:15:40 +0200

    setup project

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  imports: [],
+  templateUrl: "./app.html",
+  styleUrls: ["./app.css"],
+})
+export class App {
+  protected title = "mat-table-task";
+}
